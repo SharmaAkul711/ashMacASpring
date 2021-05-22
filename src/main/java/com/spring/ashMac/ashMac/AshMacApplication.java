fded9b9d07ad5aad87b4c093fac8d3bd773ec6b3
SharmaAkul711 <sharma.akul.711@gmail.com>
2021-05-22 14:46:10 +0530

    First Commit

---
+++ src/main/java/com/spring/ashMac/ashMac/AshMacApplication.java
@@ -0,0 +1,13 @@
+package com.spring.ashMac.ashMac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AshMacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AshMacApplication.class, args);
+	}
+
+}
